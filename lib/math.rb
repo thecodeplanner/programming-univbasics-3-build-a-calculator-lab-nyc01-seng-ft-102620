b845256e1eece6b7afa7614a85bd8fb968734a91
Melissa <thecodeplanner@gmail.com>
2020-10-11 18:37:05 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -14,11 +14,12 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  product
 end
 
 def modulo(num1, num2)
-
+  remainder = num1 % num1
 end
 
 def square_root(num)
